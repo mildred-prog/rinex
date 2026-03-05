715d567f1ffe9bb3487245bf2ce4d5f8f9164d1a
Mildred <mildylicious@gmail.com>
2026-03-05 14:08:17 +0000

    Remove unneccessary dashes to serve static correctly

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,8 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Whitenoise static file serving
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
